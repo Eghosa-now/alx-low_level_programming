01e938a10773b12e46c1eec3d6937a2bdb5fea73
Eghosa-now <eghosaidehen61@gmail.com>
2023-09-07 20:23:50 -0700

    hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#includ <stdio.h>
+
+/**
+ * main - this is the main put function
+ * Return: 0
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingua puzzle");
+        return (0);
+}
+
