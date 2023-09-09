e2192b9c551d0d82077b44d723da468506526bc7
Eghosa-now <eghosaidehen61@gmail.com>
2023-09-09 04:51:42 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program to print in reverse function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+			}
+	putchar('\n');
+	return (0);
+}
