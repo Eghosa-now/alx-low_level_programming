fc9deab26a956c6c70516dccd8ece3798440edb4
Eghosa-now <eghosaidehen61@gmail.com>
2023-11-03 17:17:44 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,25 +9,20 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_d;
-	int nletters;
-	int rwr;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	file_d = open(filename, O_WRONLY | O_APPEND0);
-	if (file_d == -1)
-		return (-1);
-	if (text_content)
+	if (text_content != NULL)
 	{
-		for (nletter = 0; text_content(nletters); nletters++)
-			;
-
-		rwr = write(file_d, text_content, nletters);
-		if (rwr == -1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	close(file_d);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	close(0);
 	return (1);
 }
